317b9f8a54842f25c9f6a305e8e5d2ad7bb914c2
justgoodluck <403028240@qq.com>
2025-02-08 23:49:51 +0800

    add handleGetTrademarkDetail,handleGetAllTrademarks

--- index.js
+++ index.js
@@ -26,10 +26,9 @@ async function handleRequestTest(req, res) {
   }
 }
 
-async function handleCompareTrademark(req, res) {
+async function handleGetTrademarkList(req, res) {
 
   const { keyword, pageIndex } = req.query;
-  console.log('------',keyword, pageIndex)
   //get Trademarks by symbol or text
   const params = {
     act: 'similar',
@@ -55,12 +54,99 @@ async function handleCompareTrademark(req, res) {
     res.status(200).json(response.data);
   } catch (error) {
     // 处理错误
-    console.error('Error fetching trademark data:', error);
+    console.error('Error fetching trademarkList data:', error);
     res.status(500).json({ error: 'Failed to fetch trademark data' });
   }
 }
 
-app.get("/handleCompareTrademark", handleCompareTrademark);
+async function handleGetTrademarkDetail(req, res) {
+  const { cls, detailId } = req.query;
+
+  const headers = {
+    'Cookie': cookie,
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    'Referer': `https://t.22.cn/Trademark/Detail/${detailId}`,
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+  };
+
+  const data = new URLSearchParams();
+  data.append('Act', 'info');
+  data.append('Cls', cls);
+  data.append('Tmid', detailId);
+
+  try {
+    // 发送Post请求
+    const response = await axios.post(`https://t.22.cn/Interface/TradeQuery.ashx`, data, {
+      headers: headers
+    });
+
+    // 返回请求结果
+    res.status(200).json(response.data);
+  } catch (error) {
+    // 处理错误
+    console.error('Error fetching trademarkDetail data:', error);
+    if (error.response) {
+      console.error('Error status:', error.response.status);
+      console.error('Error headers:', error.response.headers);
+      console.error('Error data:', error.response.data);
+    } else if (error.request) {
+      console.error('Error request:', error.request);
+    } else {
+      console.error('Error message:', error.message);
+    }
+    res.status(500).json({ error: 'Failed to fetch trademark data', details: error.message });
+  }
+}
+
+
+async function handleGetAllTrademarks(req, res) {
+  const { Ps, Pi, keyword, type, state, pageIndex } = req.query;
+
+  const headers = {
+    'Cookie': cookie,
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+  };
+
+  const data = new URLSearchParams();
+  data.append('act', 'query');
+  data.append('Ps', Ps);
+  data.append('Pi', Pi);
+  data.append('keyword', keyword);
+  data.append('type', type);
+  data.append('state', state);
+  data.append('pageIndex', pageIndex);
+
+  try {
+    // 发送Post请求
+    const response = await axios.post(`https://t.22.cn/Interface/TradeQuery.ashx`, data, {
+      headers: headers
+    });
+
+    // 返回请求结果
+    res.status(200).json(response.data);
+  } catch (error) {
+    // 处理错误
+    console.error('Error fetching trademarkDetail data:', error);
+    if (error.response) {
+      console.error('Error status:', error.response.status);
+      console.error('Error headers:', error.response.headers);
+      console.error('Error data:', error.response.data);
+    } else if (error.request) {
+      console.error('Error request:', error.request);
+    } else {
+      console.error('Error message:', error.message);
+    }
+    res.status(500).json({ error: 'Failed to fetch trademark data', details: error.message });
+  }
+}
+
+
+app.get("/handleGetTrademarkList", handleGetTrademarkList);
+
+app.get("/handleGetTrademarkDetail", handleGetTrademarkDetail);
+
+app.get("/handleGetAllTrademarks", handleGetAllTrademarks);
 
 app.get("/handleRequestTest", handleRequestTest);
 
